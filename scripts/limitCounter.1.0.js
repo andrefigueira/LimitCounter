9cc3b7bf06e8576b6a4c89cbefa7c8af90b31a2f
André Figueira <andre.figueira@me.com>
2012-10-01 20:55:02 +0100

    Added minified file and licence docblock

--- scripts/limitCounter.1.0.js
+++ scripts/limitCounter.1.0.js
@@ -1,3 +1,13 @@
+/**
+* jQuery Limit Counter 1.0
+* Developed by: Andre Figueira
+* Copyright (c) 2012 Andre Figueira
+*
+* This script is free software: you can redistribute it and/or modify it 
+* under the terms of the GNU General Public License as published by the Free 
+* Software Foundation, either version 3 of the License, or (at your option)
+* any later version. 
+*/ 
 $.fn.limitCounter = function(options)
 {
 
